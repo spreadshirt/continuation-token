5ebaa007b7cc2a1f055e40e01bb915493dd44103
Andreas Linz <anli@spreadshirt.net>
2017-11-27 14:48:37 +0100

    Validate continuation token before returning it

--- continuation-token/src/main/kotlin/net/sprd/common/continuationtoken/ContinuationTokenParser.kt
+++ continuation-token/src/main/kotlin/net/sprd/common/continuationtoken/ContinuationTokenParser.kt
@@ -11,14 +11,20 @@ internal val TOKEN_DELIMITER = ":"
 fun String.toContinuationToken(): ContinuationToken {
     val parts = this.split(TOKEN_DELIMITER)
     try {
-        return ContinuationToken(
-                timestamp = parts[0].toLong(),
-                offset = parts[1].toInt(),
-                checksum = parts[2].toLong()
-        )
+        return buildAndValidate(parts[0].toLong(), parts[1].toInt(), parts[2].toLong())
     } catch (ex: Exception) {
         throw InvalidContinuationTokenException("Invalid token '$this'.", ex)
     }
+}
+
+fun buildAndValidate(timestamp: Long, offset: Int, checksum: Long): ContinuationToken {
+    if (timestamp < 0) {
+        throw IllegalArgumentException("Timestamp is negative")
+    }
+    if (offset < 0) {
+        throw IllegalArgumentException("Checksum is negative")
+    }
+    return ContinuationToken(timestamp, offset, checksum)
 }
 
 class InvalidContinuationTokenException(message: String, cause: Exception) : RuntimeException(message, cause)
