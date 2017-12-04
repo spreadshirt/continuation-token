a57338f8bda9313891c5d6264a2db00de808d6fb
Andreas Linz <anli@spreadshirt.net>
2017-12-04 11:28:06 +0100

    Revert refactoring
    This reverts the following two refactoring:
    
    - _moving all logic in the try block_ because it hides where the
    Exception is thrown and also increases the distance of where it was
    thrown and where it is catched.
    - remove explicit ordering for every field in the SQL query. This is
    only done if fields are ordered differently

--- demo-java/src/main/java/net/sprd/demo/pagination/EmployeeResource.java
+++ demo-java/src/main/java/net/sprd/demo/pagination/EmployeeResource.java
@@ -8,7 +8,6 @@ import net.sprd.common.continuationtoken.InvalidContinuationTokenException;
 import net.sprd.common.continuationtoken.Page;
 import net.sprd.common.continuationtoken.Pagination;
 import net.sprd.common.continuationtoken.QueryAdvice;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import spark.Request;
 import spark.Response;
@@ -30,37 +29,38 @@ public class EmployeeResource {
     }
 
     public Object handle(Request request, Response response) {
+        ContinuationToken token = null;
         try {
-            ContinuationToken token = ContinuationTokenParser.toContinuationToken(request.queryParams("continue"));
-            int pageSize = getPageSizeOrDefault(request);
-
-            QueryAdvice queryAdvice = Pagination.calculateQueryAdvice(token, pageSize);
-            List<Employee> entities = jdbcTemplate.query(createQuery(queryAdvice), this::mapRow);
-
-            Page<Employee> page = Pagination.createPage(entities, token, pageSize);
-
-            EmployeePage pageDto = new EmployeePage(page);
-            response.type("application/json");
-            response.body(gson.toJson(pageDto));
-            return response.body();
-        } catch (InvalidContinuationTokenException | DataAccessException e) {
+            token = ContinuationTokenParser.toContinuationToken(request.queryParams("continue"));
+        } catch (InvalidContinuationTokenException e) {
             response.status(HTTP_BAD_REQUEST);
             return response.body();
         }
+        int pageSize = getPageSizeOrDefault(request, 10);
+
+        QueryAdvice queryAdvice = Pagination.calculateQueryAdvice(token, pageSize);
+        List<Employee> entities = jdbcTemplate.query(createQuery(queryAdvice), this::mapRow);
+
+        Page<Employee> page = Pagination.createPage(entities, token, pageSize);
+
+        EmployeePage pageDto = new EmployeePage(page);
+        response.type("application/json");
+        response.body(gson.toJson(pageDto));
+        return response.body();
     }
 
     private String createQuery(QueryAdvice queryAdvice) {
         return format("SELECT * FROM Employees" +
                 " WHERE UNIX_TIMESTAMP(timestamp) >= %d" +
-                " ORDER BY timestamp ASC, id ASC" +
+                " ORDER BY timestamp, id ASC" +
                 " LIMIT %d", queryAdvice.getTimestamp(), queryAdvice.getLimit());
     }
 
-    private int getPageSizeOrDefault(Request request) {
+    private int getPageSizeOrDefault(Request request, int defaultValue) {
         if (!Strings.isNullOrEmpty(request.queryParams("pageSize"))) {
             return Integer.valueOf(request.queryParams("pageSize"));
         }
-        return 10;
+        return defaultValue;
     }
 
     private Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
--- demo-java/src/main/java/net/sprd/demo/pagination/Main.java
+++ demo-java/src/main/java/net/sprd/demo/pagination/Main.java
@@ -3,7 +3,6 @@ package net.sprd.demo.pagination;
 import spark.Spark;
 
 public class Main {
-
     public static void main(String[] args) {
         EmployeeResource resource = new EmployeeResource(DemoSetup.init());
         Spark.get("/", resource::handle);
