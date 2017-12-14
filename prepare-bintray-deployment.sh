393429954f0f3d559b8ada05922eea6ec671070b
Andreas Linz <anli@spreadshirt.net>
2017-12-14 13:32:13 +0100

    Generate hashsums and also deploy pom.xml
    Previously we only uploaded the generated JARs without the pom.xml and
    hashsums.
    This also generates a folder called deploy that contains all files to be
    released.

--- prepare-bintray-deployment.sh
+++ prepare-bintray-deployment.sh
@@ -2,11 +2,19 @@
 
 set -euo pipefail
 
-POM_PATH="${1}"
-export POM_VERSION=$(grep --max-count=1 '<version>' "$POM_PATH" | sed -E 's/<version>(.*)<\/version>/\1/' | tr -d '[:space:]')
+PROJECT_ROOT="${1}"
+export POM_VERSION=$(grep --max-count=1 '<version>' "${PROJECT_ROOT}/pom.xml" | sed -E 's/<version>(.*)<\/version>/\1/' | tr -d '[:space:]')
 export GIT_VERSION=$(git describe --always --tags)
 export RELEASE_DATE=$(date --utc --iso-8601)
 COMMIT_MESSAGE=${TRAVIS_COMMIT_MESSAGE:-"Automated CI release"}
 export COMMIT_MESSAGE=$(echo $COMMIT_MESSAGE | tr [:space:] \ )
+
+rm -rf deploy && mkdir deploy
+cp "${PROJECT_ROOT}/pom.xml" deploy
+cp "${PROJECT_ROOT}"/target/*.jar deploy
+for f in $(ls deploy); do
+    md5sum < "deploy/$f" > "deploy/${f}.md5"
+    sha1sum < "deploy/$f" > "deploy/${f}.sha1"
+done
 
 cat bintray.json.tmpl | envsubst > bintray.json
